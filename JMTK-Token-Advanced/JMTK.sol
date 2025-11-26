34abad3f0efbfaad3fdd34cfa2a30fe1b748345b
thedarkhorse934 <william.morrison1989@proton.me>
2025-11-26 09:22:56 +0000

    Implement ERC20 token with minting and snapshot features

--- JMTK-Token-Advanced/JMTK.sol
+++ JMTK-Token-Advanced/JMTK.sol
@@ -1,1 +1,50 @@
+'''
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+/// Force OpenZeppelin v4.9.5 via GitHub tag import paths
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.5/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.5/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.5/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.5/contracts/access/AccessControl.sol";
+
+
+contract JMTK is ERC20, ERC20Burnable, ERC20Snapshot, ERC20Permit, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant SNAPSHOT_ROLE = keccak256("SNAPSHOT_ROLE");
+
+    uint256 public constant MAX_SUPPLY = 2_000_000 * 10 ** 18;
+
+    constructor()
+        ERC20("MyToken", "JMTK")
+        ERC20Permit("JMTK")
+    {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+        _grantRole(SNAPSHOT_ROLE, msg.sender);
+
+        _mint(msg.sender, 1_000_000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) external onlyRole(MINTER_ROLE) {
+        require(totalSupply() + amount <= MAX_SUPPLY, "Cap exceeded");
+        _mint(to, amount);
+    }
+
+    function snapshot() external onlyRole(SNAPSHOT_ROLE) returns (uint256) {
+        return _snapshot();
+    }
+
+    function remainingMintable() external view returns (uint256) {
+        return MAX_SUPPLY - totalSupply();
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Snapshot)
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
+
+'''
